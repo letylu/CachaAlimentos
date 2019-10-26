6e9eb24bcee8872ceb1e39e6791cf19f17ddce9e
letylu <16788208+letylu@users.noreply.github.com>
2019-10-25 20:11:47 -0500

    Update HT_Explode.cs

--- HT_Explode.cs
+++ HT_Explode.cs
@@ -5,19 +5,12 @@ using UnityEngine;
 public class HT_Explode : MonoBehaviour {
 
     public UnityEngine.GameObject explosion;
-    //public Transform explosion;
-    //public ParticleSystem[] effects;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Hat")
         {
             Instantiate(explosion, transform.position, transform.rotation);
-           // foreach (var effect in effects)
-           // {
-                //effect.transform.parent = null;
-                //effect.Stop();
-                //Destroy(effect.gameObject , 1.0f);
-            //}
             Destroy(gameObject);
         }
     }
